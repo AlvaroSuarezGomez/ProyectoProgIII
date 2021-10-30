d2d23d56b0bf50d351318551ec63b32bfa6faabf
Álvaro Suárez <alvarosuarezgomez@opendeusto.es>
2021-10-30 19:29:23 +0200

    Creado MonsterFantasy

--- MonsterFantasy/core/src/com/monsterfantasy/game/monsterfantasy.java
+++ MonsterFantasy/core/src/com/monsterfantasy/game/monsterfantasy.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MonsterFantasy extends ApplicationAdapter {
+public class monsterfantasy extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
