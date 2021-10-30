427369db113bd7a372c87ea58de48e5d27963bdd
Álvaro Suárez <alvarosuarezgomez@opendeusto.es>
2021-10-30 19:29:06 +0200

    Creado MonsterFantasy

--- MonsterFantasy/desktop/src/com/monsterfantasy/game/desktop/DesktopLauncher.java
+++ MonsterFantasy/desktop/src/com/monsterfantasy/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.monsterfantasy.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.monsterfantasy.game.MonsterFantasy;
+import com.monsterfantasy.game.monsterfantasy;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MonsterFantasy(), config);
+		new LwjglApplication(new monsterfantasy(), config);
 	}
 }
