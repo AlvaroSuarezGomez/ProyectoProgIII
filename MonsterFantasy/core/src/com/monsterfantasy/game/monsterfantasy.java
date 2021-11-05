dd47573cf7fffc49006ccae5e49ce669eb3f7bf3
Immersive-Lab-Deusto <86719736+Immersive-Lab-Deusto@users.noreply.github.com>
2021-11-05 18:47:58 +0100

    Añadida imagen de prueba

--- MonsterFantasy/core/src/com/monsterfantasy/game/monsterfantasy.java
+++ MonsterFantasy/core/src/com/monsterfantasy/game/monsterfantasy.java
@@ -1,25 +1,29 @@
 package com.monsterfantasy.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class monsterfantasy extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Camera cam = new OrthographicCamera();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("amogus.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 100, 100);
 		batch.end();
 	}
 	
