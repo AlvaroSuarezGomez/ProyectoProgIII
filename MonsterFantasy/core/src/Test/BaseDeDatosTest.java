b8468eb8d30c65cde4f1089155e985ef63e83c89
Xabier <sanzxabier@opendeusto.es>
2021-12-08 16:01:42 +0100

    Test de la base de datos correcto y finalizado

---
+++ MonsterFantasy/core/src/Test/BaseDeDatosTest.java
@@ -0,0 +1,43 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.monsterfantasy.game.battle.BaseDeDatos;
+
+public class BaseDeDatosTest {
+
+	@Before
+	public void setUp() {
+		BaseDeDatos.abrirConexion("BaseDatos", true);
+	}
+	
+	
+	@Test
+	public void testGetEnemigos() {
+		assertEquals("Zarpitas", BaseDeDatos.getEnemigos().get(0).getNombre());  //El nombre del primer enemigo se ha obtenido correctamente
+		assertEquals(3, BaseDeDatos.getEnemigos().size());
+		
+	}
+
+	@Test
+	public void testGetAtaques() {
+		assertEquals("Golpeo", BaseDeDatos.getAtaques().get(0).getNombre()); 
+		assertEquals(3, BaseDeDatos.getAtaques().size());
+	}
+
+	@Test
+	public void testGetEquipaciones() {
+		assertEquals("Espada", BaseDeDatos.getEquipaciones().get(0).getNombre()); 
+		assertEquals(3, BaseDeDatos.getEquipaciones().size());
+	}
+
+	@Test
+	public void testGetPociones() {
+		assertEquals("salud", BaseDeDatos.getPociones().get(0).getNombre()); 
+		assertEquals(3, BaseDeDatos.getPociones().size());
+	}
+
+}
