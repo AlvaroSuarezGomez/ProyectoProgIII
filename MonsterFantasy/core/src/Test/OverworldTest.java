bd4c6439451445ac4ada9f33e0b091cd7019a3b5
alvarosuarezgomez <alvarosuarezgomez@opendeusto.es>
2022-01-14 20:08:44 +0100

    Arreglos para build

--- MonsterFantasy/core/src/Test/OverworldTest.java
+++ MonsterFantasy/core/src/Test/OverworldTest.java
@@ -10,14 +10,14 @@ public class OverworldTest {
 
 	Overworld overworldTest = new Overworld();
 	
-	//@Test
-	/*public void crearCeldasTest() {
+	@Test
+	public void crearCeldasTest() {
 		overworldTest.crearCeldas();
 		for (int row = 0; row < overworldTest.getFilas(); row++) {
 			for (int column = 0; column < overworldTest.getColumnas(); column++) {
 				assertNotEquals(null, overworldTest.getCeldas()[row][column]);
 			}
 		}
-	}*/
+	}
 
 }
